a845771ce5483197cd2c1457074bb4d92c275070
Fidogreg <rafael.moretti@escola.pr.gov.br>
2024-05-07 09:55:33 -0300

    Feat add Tipos boleanos

---
+++ typeBooleans.js
@@ -0,0 +1,18 @@
+//Boolean
+//Verdadeiro ou falso
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+// === Compara tipo e valor
+console.log(primeiroNumero === segundoNumero);
+
+// Declarar uma variavel com um valor booleano
+const ativo = falso;
+let ativo2 = true;// vai permitir a mudanca do valor da variavel
+
+const nome = " Rafael";
+const sobrenome = "Fernandes";
+
+console.log(nome === sobrenome);
+
