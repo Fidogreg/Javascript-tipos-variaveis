38f2ca8277ac5cc83688eff41ab0cafc90c099d1
Fidogreg <rafael.moretti@escola.pr.gov.br>
2024-05-17 09:58:35 -0300

    Adicionado Truthy e falsy

---
+++ truthfalsy.js
@@ -0,0 +1,21 @@
+// Truth e Falsy sao tipos primitivos de comparacao tambem
+// 0 e 1
+
+console.log(0 == falso);
+console.log(1 == true);
+console.log(0 == true);
+console.log(1 == false);
+
+console.log("" == falso);
+
+let minhaVar;
+let testeNull = null;
+
+console.log(minhaVar);
+console.log(testeNull);
+
+let numero = 3;
+let teste = "Alura";
+
+console.log(typeof[minhaVar]);
+console.log(typeof[testeNull]);
